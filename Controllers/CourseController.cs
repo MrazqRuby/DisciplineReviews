556e3567180af0d6586fc2a73fe8430c9a8a155c
Siyana <siyanaslavova@gmail.com>
2013-10-26 20:17:18 +0300

    add ajax request for reviews

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -17,6 +17,30 @@ namespace DisciplineReviews.Controllers
             Course course = new Course();
             return View(course);
         }
+        public ActionResult _CourseView(int id)
+        {
+            Course model = BusinessLogic.GetCourse(id);
+            return PartialView(model);
+        }
 
+        public ActionResult _CourseRating(Course model)
+        {
+            return PartialView(model);
+        }
+
+        public ActionResult _CourseBody(Course model)
+        {
+            return PartialView(model);
+        }
+
+        public ActionResult _CourseOverview(Course model)
+        {
+            return PartialView(model);
+        }
+
+        public ActionResult _CourseReview(Course model)
+        {
+            return PartialView(model);
+        }
     }
 }
--- Models/Course.cs
+++ Models/Course.cs
@@ -8,13 +8,19 @@ namespace DisciplineReviews.Models
     public class Course
     {
         public int Id { get; set; }
+
         public string CourseName { get; set; }
         public string LecturerName { get; set; }
+
         public decimal TotalRating { get; set; }
+
         public decimal Helpfulness { get; set; }
         public decimal Easiness { get; set; }
         public decimal Clarity { get; set; }
+        public decimal Interest { get; set; }
         public decimal WorkLoad { get; set; }
+
         public decimal AvgGrade { get; set; }
+
     }
 }
